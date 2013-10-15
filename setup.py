0dffa96899d52312affaecef18d7984b8b33e182
Bengt Lüers <bengt.lueers@gmail.com>
2013-10-16 00:00:44 +0200

    Simpler include of the LICENSE file

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages, Extension
-import os
 
 hang_module_ext = Extension('orges.test.unit.hang', 
   sources = ['orges/test/unit/hangmodule.c'])
@@ -14,7 +13,7 @@ setup(
   author='Renke Grunwald, Bengt Lüers, Jendrik Poloczek',
   author_email='info@orges.org',
   url='http://organic-es.tumblr.com/',
-  license=open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "LICENSE")).read(),
+  license=open("LICENSE").read(),
   packages=find_packages(exclude=('tests', 'docs')),
   ext_modules = [hang_module_ext],
 )
