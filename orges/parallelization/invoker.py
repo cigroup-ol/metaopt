bd5978ab99bde08406dbe0799c3e7fb5db4002d9
Renke Grunwald <renke@renke.org>
2013-10-21 11:53:22 +0200

    Add invoker class

---
+++ orges/parallelization/invoker.py
@@ -0,0 +1,17 @@
+from orges.args import call
+
+class Invoker(object):
+    def __init__(self, resources):
+        pass
+
+    def subinvoker(resources):
+        pass
+
+    def invoke(f, args, **vargs):
+        # ret = call(f, args)
+        # caller.on_result(args, **vargs)
+
+    def wait():
+        pass
+        # Warten bis für jeden Aufruf von invoke einmal on_result oder on_error
+        # aufgerufen wurde
