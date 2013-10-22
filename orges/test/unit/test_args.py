61c8a0212d037a3575cb5c4878e23058e04a7479
Renke Grunwald <renke@renke.org>
2013-10-22 13:29:53 +0200

    Change PARAM_SPEC to param_spec since it's a local

--- orges/test/unit/test_args.py
+++ orges/test/unit/test_args.py
@@ -14,12 +14,12 @@ from orges.args import CallNotPossibleError
 from orges.args import call
 
 def test_call_func_with_args_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
-    PARAM_SPEC.int("b")
+    param_spec = ParamSpec()
+    param_spec.int("a")
+    param_spec.int("b")
 
-    param_a = PARAM_SPEC.params["a"]
-    param_b = PARAM_SPEC.params["b"]
+    param_a = param_spec.params["a"]
+    param_b = param_spec.params["b"]
 
     arg_a = Arg(param_a, 0)
     arg_b = Arg(param_b, 1)
@@ -35,10 +35,10 @@ def test_call_func_with_args_works():
 
 
 def test_call_func_with_single_arg_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
+    param_spec = ParamSpec()
+    param_spec.int("a")
 
-    param_a = PARAM_SPEC.params["a"]
+    param_a = param_spec.params["a"]
 
     arg_a = Arg(param_a, 0)
 
@@ -53,12 +53,12 @@ def test_call_func_with_single_arg_works():
 
 
 def test_call_func_with_dict_args_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
-    PARAM_SPEC.int("b")
+    param_spec = ParamSpec()
+    param_spec.int("a")
+    param_spec.int("b")
 
-    param_a = PARAM_SPEC.params["a"]
-    param_b = PARAM_SPEC.params["b"]
+    param_a = param_spec.params["a"]
+    param_b = param_spec.params["b"]
 
     arg_a = Arg(param_a, 0)
     arg_b = Arg(param_b, 1)
@@ -74,12 +74,12 @@ def test_call_func_with_dict_args_works():
 
 
 def test_call_func_with_kwargs_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
-    PARAM_SPEC.int("b")
+    param_spec = ParamSpec()
+    param_spec.int("a")
+    param_spec.int("b")
 
-    param_a = PARAM_SPEC.params["a"]
-    param_b = PARAM_SPEC.params["b"]
+    param_a = param_spec.params["a"]
+    param_b = param_spec.params["b"]
 
     arg_a = Arg(param_a, 0)
     arg_b = Arg(param_b, 1)
@@ -95,12 +95,12 @@ def test_call_func_with_kwargs_works():
 
 
 def test_call_func_with_args_returns_result():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
-    PARAM_SPEC.int("b")
+    param_spec = ParamSpec()
+    param_spec.int("a")
+    param_spec.int("b")
 
-    param_a = PARAM_SPEC.params["a"]
-    param_b = PARAM_SPEC.params["b"]
+    param_a = param_spec.params["a"]
+    param_b = param_spec.params["b"]
 
     arg_a = Arg(param_a, 0)
     arg_b = Arg(param_b, 1)
@@ -113,12 +113,12 @@ def test_call_func_with_args_returns_result():
 
 @raises(CallNotPossibleError)
 def test_call_func_with_vargs_raises_error():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
-    PARAM_SPEC.int("b")
+    param_spec = ParamSpec()
+    param_spec.int("a")
+    param_spec.int("b")
 
-    param_a = PARAM_SPEC.params["a"]
-    param_b = PARAM_SPEC.params["b"]
+    param_a = param_spec.params["a"]
+    param_b = param_spec.params["b"]
 
     arg_a = Arg(param_a, 0)
     arg_b = Arg(param_b, 1)
@@ -131,12 +131,12 @@ def test_call_func_with_vargs_raises_error():
 
 @raises(CallNotPossibleError)
 def test_call_func_with_incorrect_number_of_args_raises_error():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a")
-    PARAM_SPEC.int("b")
+    param_spec = ParamSpec()
+    param_spec.int("a")
+    param_spec.int("b")
 
-    param_a = PARAM_SPEC.params["a"]
-    param_b = PARAM_SPEC.params["b"]
+    param_a = param_spec.params["a"]
+    param_b = param_spec.params["b"]
 
     arg_a = Arg(param_a, 0)
     arg_b = Arg(param_b, 1)
@@ -148,73 +148,73 @@ def test_call_func_with_incorrect_number_of_args_raises_error():
 
 
 def test_arg_iter_bounded_int_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a").interval((1, 10))
+    param_spec = ParamSpec()
+    param_spec.int("a").interval((1, 10))
 
-    values = [arg.value for arg in list(Arg(PARAM_SPEC.params["a"]))]
+    values = [arg.value for arg in list(Arg(param_spec.params["a"]))]
     eq_(values, range(1, 11))
 
 
 def test_arg_iter_bounded_int_small_interval_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a").interval((1, 2))
+    param_spec = ParamSpec()
+    param_spec.int("a").interval((1, 2))
 
-    values = [arg.value for arg in list(Arg(PARAM_SPEC.params["a"]))]
+    values = [arg.value for arg in list(Arg(param_spec.params["a"]))]
     eq_(values, [1, 2])
 
 
 def test_arg_iter_bounded_int_with_step_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a").interval((1, 10)).step(2)
+    param_spec = ParamSpec()
+    param_spec.int("a").interval((1, 10)).step(2)
 
-    values = [arg.value for arg in list(Arg(PARAM_SPEC.params["a"]))]
+    values = [arg.value for arg in list(Arg(param_spec.params["a"]))]
     # TODO: Should the upper bound always be included?
     eq_(values, [1, 3, 5, 7, 9, 10])
 
 
 def test_arg_iter_bool_works():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.bool("a")
+    param_spec = ParamSpec()
+    param_spec.bool("a")
 
-    values = [arg.value for arg in list(BoolArg(PARAM_SPEC.params["a"]))]
+    values = [arg.value for arg in list(BoolArg(param_spec.params["a"]))]
     eq_(values, [True, False])
 
 
 @raises(UnboundedArgIterError)
 def test_arg_iter_unbounded_raises_error():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.float("a")
+    param_spec = ParamSpec()
+    param_spec.float("a")
 
-    list(Arg(PARAM_SPEC.params["a"]))
+    list(Arg(param_spec.params["a"]))
 
 
 @raises(UnboundedArgIterError)
 def test_arg_iter_half_bounded_raises_error():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.float("a").interval((0, None))
-    PARAM_SPEC.float("b").interval((None, 0))
+    param_spec = ParamSpec()
+    param_spec.float("a").interval((0, None))
+    param_spec.float("b").interval((None, 0))
 
-    list(Arg(PARAM_SPEC.params["a"]))
-    list(Arg(PARAM_SPEC.params["b"]))
+    list(Arg(param_spec.params["a"]))
+    list(Arg(param_spec.params["b"]))
 
 
 @raises(NoStepArgIterError)
 def test_arg_iter_no_step_raises_error():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.float("a").interval((0, 1))
-    list(Arg(PARAM_SPEC.params["a"]))
+    param_spec = ParamSpec()
+    param_spec.float("a").interval((0, 1))
+    list(Arg(param_spec.params["a"]))
 
 
 def test_arg_repr_no_display_name_shows_name_and_value():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a").interval((0, None))
-    eq_(str(Arg(PARAM_SPEC.params["a"])), "a=0")
+    param_spec = ParamSpec()
+    param_spec.int("a").interval((0, None))
+    eq_(str(Arg(param_spec.params["a"])), "a=0")
 
 
 def test_arg_repr_display_name_shows_display_name_and_value():
-    PARAM_SPEC = ParamSpec()
-    PARAM_SPEC.int("a", "α").interval((0, None))
-    eq_(str(Arg(PARAM_SPEC.params["a"])), "α=0")
+    param_spec = ParamSpec()
+    param_spec.int("a", "α").interval((0, None))
+    eq_(str(Arg(param_spec.params["a"])), "α=0")
 
 
 if __name__ == '__main__':
