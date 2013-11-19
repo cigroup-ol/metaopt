c7e49e2159cf24e6abed71d49531a153fa5aef19
Renke Grunwald <renke@renke.org>
2013-11-19 12:58:35 +0100

    Change InvocationPlugin to be a "normal" class
    Plugins only have to override the methods they are interested in.

--- orges/invoker/pluggable.py
+++ orges/invoker/pluggable.py
@@ -178,33 +178,27 @@ class Invocation(object):
 
 class InvocationPlugin(object):
     """
-    Abstract base class for invocation plug-ins.
+    Base class for invocation plug-ins.
     """
 
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
     def before_invoke(self, invocation):
         """
         Gets called when plugpable invoker starts preparing a calls to invoke.
         """
         pass
 
-    @abstractmethod
     def on_invoke(self, invocation):
         """
         Gets called right before pluggable invoker calls invoke on its invoker.
         """
         pass
 
-    @abstractmethod
     def on_result(self, invocation):
         """
         Gets called when pluggable invoker receives a callback to on_result.
         """
         pass
 
-    @abstractmethod
     def on_error(self, invocation):
         """
         Gets called when pluggable invoker receives a callback to on_error.
