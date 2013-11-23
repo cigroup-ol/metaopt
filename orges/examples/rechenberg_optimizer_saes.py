0b49b3f8fdf835b029529595ef161510862b8af3
Renke Grunwald <renke@renke.org>
2013-11-23 15:22:11 +0100

    Add new RechenbergOptimizer
    Also fix a small bug in the SAESOptimizer.

---
+++ orges/examples/rechenberg_optimizer_saes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, with_statement
+
+from orges.plugins.print import PrintInvocationPlugin
+from orges.main import optimize
+from orges.optimizer.rechenberg import RechenbergOptimizer
+import orges.param as param
+from orges.test.demo.algorithm.host.saes import f as saes
+
+
+@param.int("mu", interval=(5, 10), display_name="μ")
+@param.int("lambd", interval=(5, 10), display_name="λ")
+@param.float("tau0", interval=(0, 1), step=0.5, display_name="τ0")
+@param.float("tau1", interval=(0, 1), step=0.5, display_name="τ1")
+def f(mu, lambd, tau0, tau1):
+    args = dict()
+
+    args["d"] = 2
+    args["epsilon"] = 0.0001
+    args["mu"] = mu
+    args["lambd"] = lambd
+    args["tau0"] = tau0
+    args["tau1"] = tau1
+
+    return saes(args)
+
+if __name__ == '__main__':
+    plugins = [PrintInvocationPlugin()]
+
+    print(optimize(function=f, optimizer=RechenbergOptimizer(),
+                   plugins=plugins))
