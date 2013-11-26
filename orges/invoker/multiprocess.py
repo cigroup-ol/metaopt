1c43a339892b47576a46ca48f7cae9a30585aa79
Bengt Lüers <bengt.lueers@gmail.com>
2013-11-26 01:07:58 +0100

    Move multiprocess' utilities to own module

--- orges/invoker/multiprocess.py
+++ orges/invoker/multiprocess.py
@@ -3,18 +3,15 @@ Invoker that uses multiple processes.
 """
 from __future__ import division, print_function, with_statement
 
-from collections import namedtuple
 import logging
 import multiprocessing
 from multiprocessing import Queue, cpu_count
-from multiprocessing import Process
-from orges.args import call
+import uuid
 
 from orges.invoker.base import BaseInvoker
-import uuid
-import inspect
-import sys
-import os
+
+from orges.invoker.multiprocess_util import WorkerProvider, Task, TaskHandle, \
+    Status, determine_package
 
 # TODO use Pool from multiprocess
 # TODO ensure tasks can be cancelled that are waiting in the queue
@@ -54,33 +51,12 @@ class MultiProcessInvoker(BaseInvoker):
         self._queue_tasks = Queue()
 
         # init worker processes
-        self._worker_processes = None
-        self._populate_worker_processes()
+        self._worker_processes = []
+        self._worker_provider = WorkerProvider()
+        #self._populate_worker_processes()
 
         super(MultiProcessInvoker, self).__init__(resources=resources,
                                                   caller=self._caller)
-
-    def _populate_worker_processes(self):
-        """
-        Fills this class with as many worker processes with as many as allowed.
-        """
-        self._worker_processes = []
-        for worker_id in range(self.worker_count_max):
-            self._worker_processes.append(self._get_worker_process(worker_id))
-
-    def _provision_worker(self):
-        if len(self._worker_processes) is not self.worker_count_max:
-            worker_id = uuid.uuid4()
-            self._worker_processes.append(self._get_worker_process(worker_id))
-
-    def _get_worker_process(self, worker_id):
-        worker_process = WorkerProcess(worker_id=worker_id,
-                             queue_tasks=self._queue_tasks,
-                             queue_results=self._queue_results,
-                             queue_status=self._queue_status)
-        worker_process.daemon = True  # disallow workers to spawn processes
-        worker_process.start()
-        return worker_process
 
     @property
     def caller(self):
@@ -100,40 +76,40 @@ class MultiProcessInvoker(BaseInvoker):
         """
         Puts a task into this Invoker's task queue for the workers to execute.
         """
-
         #self._logger.warning("invoke entered")
 
-        self._provision_worker()
+        # provision a new worker, if allowed
+        if len(self._worker_processes) is not self.worker_count_max:
+            process = self._worker_provider.provision(
+                   queue_tasks=self._queue_tasks,
+                   queue_results=self._queue_results,
+                   queue_status=self._queue_status)
+            self._worker_processes += process
 
-        # call by package:
-        # - Determine absolute package name of the given function.
-        # - When the task gets executed, the worker process will import it.
-
-        # expand the module's path to an absolute import
-        filename = inspect.getsourcefile(function)
-        module_path, module_filename = os.path.split(filename)
-        module_name, module_ext = os.path.splitext(module_filename)
-        prefix = []
-        for dir in module_path.split(os.sep)[::-1]:
-            prefix.append(dir)
-            candidate = ".".join(prefix[::-1] + [module_name])
-            #print(candidate)
-            try:
-                __import__(candidate, globals(), locals(), [], 0)
-                f_package = candidate
-                break
-            except ImportError:
-                pass
-
-        task_id = uuid.uuid4()
-        self._queue_tasks.put(Task(task_id=task_id, f_package=f_package,
+        # schedule task
+        self._queue_tasks.put(Task(task_id=uuid.uuid4(),
+                                   f_package=determine_package(function),
                                    args=fargs, vargs=vargs))
+
+        # wait for status
         status = self._queue_status.get()
         if status is None:
             self.aborted = True
             task_handle = None
         else:
             task_handle = TaskHandle(self, status.worker_id, status.task_id)
+
+        # wait for result, if at worker limit
+        if len(self._worker_processes) is self.worker_count_max:
+            result = self._queue_results.get()
+
+            # handle finished worker
+            if result.value is None:
+                worker_process = self._worker_processes[result.worker_id]
+                self._remove_worker_process(worker_process)
+
+            # handle all other results
+            self._caller.on_result(result.value, result.args, **result.vargs)
 
         #self._logger.warning("invoke left")
 
@@ -184,17 +160,7 @@ class MultiProcessInvoker(BaseInvoker):
 
         while self._count_busy_workers() >= 1:
             #self._logger.warning(self._count_busy_workers())
-            # wait for the next result from the queue
-            result = self._queue_results.get()
-
-            # handle finished worker
-            if result.value == None:
-                worker_process = self._worker_processes[result.worker_id]
-                self._remove_worker_process(worker_process)
-                break
-
-            # handle all other results
-            self._caller.on_result(result.value, result.args, **result.vargs)
+            pass
 
     def _remove_worker_process(self, worker_process):
         status = Status(worker_id=worker_process.worker_id, f_package=None,
@@ -226,104 +192,3 @@ class MultiProcessInvoker(BaseInvoker):
         """Reports the status of the workforce."""
         # TODO implement me
         pass
-
-
-class TaskHandle(object):
-    def __init__(self, invoker, worker_id, task_id):
-        self._invoker = invoker
-        self._worker_id = worker_id
-        self.task_id = task_id
-
-    def cancel(self):
-        self._invoker.cancel(self._worker_id, self.task_id)
-
-
-# Datastructure for results generated by the workers
-Task = namedtuple("Task", ["task_id", "f_package", "args", "vargs"])
-
-# Datastructure for results generated by the workers
-Status = namedtuple("Status", ["task_id", "f_package", "args", "vargs",
-                               "worker_id"])
-
-# Datastructure for results generated by the workers
-Result = namedtuple("Result", ["task_id", "f_package", "args", "vargs",
-                               "worker_id", "value"])
-
-
-class WorkerProcess(Process):
-    """Calls functions with arguments, both given by a queue."""
-    def __init__(self, worker_id, queue_tasks, queue_results, queue_status):
-        self._worker_id = worker_id
-        self._queue_tasks = queue_tasks
-        self._queue_results = queue_results
-        self._queue_status = queue_status
-        self._busy = False
-        self._current_task_id = None
-        super(WorkerProcess, self).__init__()
-
-    @property
-    def worker_id(self):
-        """Property for the worker_id attribute of this class."""
-        return self._worker_id
-
-    @property
-    def queue_tasks(self):
-        """Property for the tasks attribute of this class."""
-        return self._queue_tasks
-
-    @property
-    def queue_status(self):
-        """Property for the results attribute of this class."""
-        return self._queue_status
-
-    @property
-    def queue_results(self):
-        """Property for the results attribute of this class."""
-        return self._queue_results
-
-    @property
-    def busy(self):
-        """Property for the results attribute of this class."""
-        return self._busy
-
-    @property
-    def current_task_id(self):
-        """Property for the results attribute of this class."""
-        return self._current_task_id
-
-    def run(self):
-        for task in iter(self.queue_tasks.get, None):
-            self._busy = True
-            self._current_task_id = task.task_id
-            # announce start of work
-            #multiprocessing.log_to_stderr(logging.WARN).warning("a")
-            status = Status(task_id=self._current_task_id,
-                             worker_id=self._worker_id,
-                            f_package=task.f_package,
-                            args=task.args, vargs=task.vargs)
-            self._queue_status.put(status)
-
-            # import f given by qualified package name
-            f = __import__(task.f_package, globals(), locals(), ['f'], 0).f
-            # Note that the following is equivalent:
-            #from orges.test.integration.invoker.multiprocess import f as f
-
-            # the actual call
-            value = call(f, task.args)
-
-            #multiprocessing.log_to_stderr(logging.WARN).warning("b")
-
-            # report result back
-            self._queue_results.put(Result(task_id=self._current_task_id,
-                                           worker_id=self._worker_id,
-                                           f_package=task.f_package,
-                                           args=task.args, value=value,
-                                           vargs=task.vargs))
-            #multiprocessing.log_to_stderr(logging.WARN).warning("c")
-            self._busy = False
-        # send sentinel back
-        #multiprocessing.log_to_stderr(logging.WARN).warning("d")
-        self._queue_results.put(Result(id=self._worker_id, f=None,
-                                          args=None, vargs=None, value=None))
-        #multiprocessing.log_to_stderr(logging.WARN).warning("e")
-
