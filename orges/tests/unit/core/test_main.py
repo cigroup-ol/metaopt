1369b8534e449f2412ba50972d7170f7cb8c7a37
Bengt Lüers <bengt.lueers@gmail.com>
2014-01-14 15:15:30 +0100

    fix test for main by adding an invoker

--- orges/tests/unit/core/test_main.py
+++ orges/tests/unit/core/test_main.py
@@ -7,6 +7,7 @@ from __future__ import division, print_function, with_statement
 from nose.tools import raises
 
 from orges.core.main import custom_optimize, NoParamSpecError
+from orges.invoker.simple import SimpleInvoker
 
 
 def f(x, y):
@@ -15,7 +16,7 @@ def f(x, y):
 
 @raises(NoParamSpecError)
 def test_custom_optimize_given_no_param_spec_complains():
-    custom_optimize(f)
+    custom_optimize(f, SimpleInvoker())
 
 if __name__ == '__main__':
     import nose
