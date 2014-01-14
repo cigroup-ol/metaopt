c2af4ce7697003409a35ba19528e7c595d366b5e
Bengt Lüers <bengt.lueers@gmail.com>
2014-01-14 02:18:03 +0100

    next MultiProcessInvoker and respective tests

--- orges/invoker/multiprocess.py
+++ orges/invoker/multiprocess.py
@@ -4,75 +4,66 @@ Invoker that uses multiple processes.
 from __future__ import division, print_function, with_statement
 
 import uuid
-import logging
-import multiprocessing
+from time import sleep
 from threading import Lock
-from multiprocessing import Queue, cpu_count
+from multiprocessing import Manager
 
 from orges.invoker.base import BaseInvoker
+from orges.util.stoppable import stopping_method, stoppable_method
 from orges.invoker.util.task_handle import TaskHandle
-from orges.invoker.util.worker_provider import Task, WorkerProcessProvider
+from orges.invoker.util.worker_provider import Task, Error, Result, \
+    WorkerProcessProvider
 from orges.invoker.util.determine_package import determine_package
-
-# TODO use Pool from multiprocess?
-# TODO ensure tasks can be cancelled that are waiting in the queue
+from orges.invoker.util.determine_worker_count import determine_worker_count
 
 
 class MultiProcessInvoker(BaseInvoker):
-    """Invoker that invokes objective functions in parallel"""
+    """
+    Invoker that invokes objective functions in parallel using processes.
+    """
 
     def __init__(self, resources=None):
         """
-        :param  resources: number of CPUs to use. will automatically configure itself, if None
+        :param  resources: Number of CPUs to use at most. Will automatically
+                           configure itself, if None.
         """
 
-        # spawn one worker process per CPU, or as many as requested
-        if resources is None:
-            try:
-                self._worker_count_max = cpu_count()  # configure automatically
-            except NotImplementedError:
-                self._worker_count_max = 2    # dual cores are very common, now
-        else:
-            self._worker_count_max = resources
-
-        self._aborted = False
+        self._worker_count_max = determine_worker_count(resources)
 
         # set this using the property
         self._caller = None
 
         # queues common to all worker processes
-        self._queue_results = Queue()
-        self._queue_status = Queue()
-        self._queue_tasks = Queue()
+        manager = Manager()
+        self._queue_results = manager.Queue()
+        self._queue_status = manager.Queue()
+        self._queue_tasks = manager.Queue()
 
         self._worker_handles = []
         self._worker_provider = WorkerProcessProvider()
 
         # initialize logging
-        self._logger = multiprocessing.log_to_stderr(logging.INFO)
+        #self._logger = multiprocessing.log_to_stderr(logging.INFO)
 
         # we can not prohibit others to use us in parallel, so
-        # make this thing thread-safe
+        # make this invoker thread-safe
         self._lock = Lock()
 
-        super(MultiProcessInvoker, self).__init__(self._caller)
+        super(MultiProcessInvoker, self).__init__(self.caller)
 
     @property
     def caller(self):
-        """Gets the caller."""
+        """Property for the private caller attribute."""
         with self._lock:
             return self._caller
 
     @caller.setter
     def caller(self, value):
-        """Sets the caller."""
+        """Setter for the private caller attribute."""
         with self._lock:
             self._caller = value
 
-    def get_subinvoker(self, resources):
-        """Returns a subinvoker using the given amount of resources of self."""
-        raise NotImplementedError()  # TODO implement
-
+    @stoppable_method
     def invoke(self, function, fargs, *vargs, **kwargs):
         """
         Invokes call(f, fargs) with the given function and the given arguments.
@@ -83,34 +74,35 @@ class MultiProcessInvoker(BaseInvoker):
         executed immediately, especially when using multiple processes/threads.
         """
         with self._lock:
-            # try to provision a new worker
+            # try to provision one new worker
             try:
-                self._worker_handles += self._worker_provider.provision(
+                self._worker_handles.append(self._worker_provider.provision(
                     queue_tasks=self._queue_tasks,
                     queue_results=self._queue_results,
-                    queue_status=self._queue_status)
+                    queue_status=self._queue_status))
             except IndexError:
                 pass  # give the task to one of the busy workers
 
-            # schedule task for the workers to execute
+            # schedule task for any worker to execute
             self._queue_tasks.put(Task(task_id=uuid.uuid4(),
                                        function=determine_package(function),
                                        args=fargs, vargs=vargs, kwargs=kwargs))
 
-            # wait for status from a worker
+            # wait for any worker to start working on the task
             status = self._queue_status.get()
+
             if status is None:
-                self._aborted = True
+                # WTF is going on here?
                 task_handle = None
             else:
-                task_handle = TaskHandle(self, status.task_id)
+                task_handle = TaskHandle(invoker=self, task_id=status.task_id)
 
-            # wait for result, if at worker limit
+            # if at worker limit, wait for an result
             if len(self._worker_handles) is self._worker_count_max:
                 result = self._queue_results.get()
 
                 # handle finished worker
-                if result.value is None:
+                if result is None:
                     for worker_handle in self._worker_handles:
                         if (
                                 worker_handle.worker_id == result.worker_id
@@ -119,55 +111,48 @@ class MultiProcessInvoker(BaseInvoker):
                             worker_handle.stop()
                             self._worker_handles.remove(worker_handle)
 
-                # handle all other results
-                print(result.value.value)
-                raise Exception
+                # handle regular results
+                if isinstance(result, Result):
+                    self._caller.on_result(result=result.value,
+                                           fargs=result.args,
+                                           vargs=result.vargs,
+                                           kwargs=result.kwargs)
+                # handle error results
+                if isinstance(result, Error):
+                    self._caller.on_error(result=result.value,
+                                           fargs=result.args,
+                                           vargs=result.vargs,
+                                           kwargs=result.kwargs)
 
-                self._caller.on_result(result=result.value, fargs=result.args,
-                                       vargs=result.vargs,
-                                       kwargs=result.kwargs)
-                # TODO on_error
+            return task_handle
 
-            return task_handle, self._aborted
-
-    def abort(self):
+    @stoppable_method
+    @stopping_method
+    def stop(self):
         """Terminates all worker processes for immediate shutdown."""
-
-        # shutdown all workers
         with self._lock:
             for worker_handle in self._worker_handles:
                 worker_handle.stop()
                 self._worker_handles.remove(worker_handle)
-            self._aborted = True
+            return
 
-            value = 0
-            return (self._aborted, value)
-
-    def terminate_gracefully(self):
-        """Sends sentinel objects to all workers to allow clean shutdown."""
-
-        self.abort()
-
-    def wait(self):
-        """
-        Blocks till all invoke, on_error or on_result calls are done. Listens
-        to the result queue for all workers and dispatches handling.
-        """
-
-        self.terminate_gracefully()
-
-    def stop(self, worker_id, task_id):
+    def stop_worker(self, worker_id, task_id):
         """Terminates a worker_handle given by id."""
-
         with self._lock:
             for worker_handle in self._worker_handles:
                 if worker_handle.worker_id == worker_id and \
                         worker_handle.current_task_id == task_id:
                     worker_handle.stop()
                     self._worker_handles.remove(worker_handle)
-                    return  # handle is unique, don't look any further
-        # TODO: handle queue corruption, by swapping them out for new ones?
+                    return  # worker handle is unique, don't look any further
 
-    def status(self):
-        """Reports the status of the work force."""
-        raise NotImplementedError()  # TODO implement
+    def wait(self):
+        """Blocks until all tasks are done."""
+        busy = True
+        while busy:
+            busy = False
+            for handle in self._worker_handles:
+                if handle.busy:
+                    busy = True
+                    sleep(0.1)
+                    break
--- orges/tests/integration/invoker/test_multiprocess.py
+++ orges/tests/integration/invoker/test_multiprocess.py
@@ -5,79 +5,75 @@ from __future__ import division, print_function, with_statement
 
 from mock import Mock
 
-from orges.core import param
 from orges.core.args import ArgsCreator
-from orges.invoker.multiprocess import MultiProcessInvoker
+from orges.tests.util.functions import FUNCTIONS_INTEGER_FAILING, \
+    FUNCTIONS_INTEGER_WORKING
+from orges.invoker.multiprocess2 import MultiProcessInvoker
+from orges.invoker.util.determine_package import determine_package
 from orges.tests.integration.invoker.util import EqualityMatcher as Matcher
 
 
-@param.int("a", interval=(1, 10))
-@param.int("b", interval=(1, 10))
-def f(a, b):
-    return a + b
-
-F_PARAM_SPEC = f.param_spec
-F_ARGS = ArgsCreator(F_PARAM_SPEC).args()
-
-
 def test_invoke_calls_on_result():
-    caller = Mock()
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+    for function in FUNCTIONS_INTEGER_WORKING[:1]:  # TODO test all functions
+        print(function, determine_package(function))
 
-    invoker = MultiProcessInvoker(resources=1)
-    invoker.caller = caller
+        caller = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
+        invoker = MultiProcessInvoker(resources=1)
+        invoker.caller = caller
 
-    invoker.invoke(f, F_ARGS)
-    invoker.wait()
+        invoker.invoke(function, ArgsCreator(function.param_spec).args())
+        invoker.wait()
 
-    assert not caller.on_error.called
-    caller.on_result.assert_called_once_with(fargs=Matcher(F_ARGS),
-                                             result=Matcher(2),
-                                             vargs=(),
-                                             kwargs={})
+        assert not caller.on_error.called
+        caller.on_result.assert_called_once_with(
+                fargs=Matcher(ArgsCreator(function.param_spec).args()),
+                result=Matcher(0),
+                vargs=(),
+                kwargs={})
 
 
 def test_invoke_given_extra_args_calls_on_result_with_them():
-    caller = Mock()
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+    for function in FUNCTIONS_INTEGER_WORKING[:1]:  # TODO test all functions
+        print(function, determine_package(function))
 
-    invoker = MultiProcessInvoker(resources=1)  # TODO fails in parallel
-    invoker.caller = caller
+        caller = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
 
-    data = dict()
-    invoker.invoke(f, F_ARGS, data=data)
-    invoker.wait()
+        invoker = MultiProcessInvoker(resources=1)  # TODO fails in parallel
+        invoker.caller = caller
 
-    assert not caller.on_error.called
-    caller.on_result.assert_called_once_with(fargs=Matcher(F_ARGS),
-                                             result=Matcher(2),
-                                             vargs=(), kwargs=Matcher(data))
+        data = dict()
+        invoker.invoke(function, ArgsCreator(function.param_spec).args(),
+                       data=data)
+        invoker.wait()
 
-
-@param.int("a", interval=(1, 10))
-@param.int("b", interval=(1, 10))
-def failing(a, b):
-    raise Exception()
-
-FAILING_PARAM_SPEC = failing.param_spec
-FAILING_ARGS = ArgsCreator(FAILING_PARAM_SPEC).args()
+        assert not caller.on_error.called
+        caller.on_result.assert_called_once_with(
+                fargs=Matcher(ArgsCreator(function.param_spec).args()),
+                result=Matcher(0),
+                vargs=(),
+                kwargs=Matcher(data))
 
 
 def test_invoke_not_successful_calls_on_error():
-    caller = Mock()
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+    for function in FUNCTIONS_INTEGER_FAILING[:1]:  # TODO test all functions
+        print(function, determine_package(function))
 
-    invoker = MultiProcessInvoker(resources=1)  # TODO fails in parallel
-    invoker.caller = caller
+        caller = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
+        invoker = MultiProcessInvoker(resources=1)  # TODO fails in parallel
+        invoker.caller = caller
 
-    data = dict()
-    invoker.invoke(f, FAILING_ARGS, data=data)
-    invoker.wait()
+        data = dict()
+        invoker.invoke(function, ArgsCreator(function.param_spec).args(),
+                       data=data)
+        invoker.wait()
 
-    assert caller.on_error.called
+        assert caller.on_error.called
 
 if __name__ == '__main__':
     import nose
