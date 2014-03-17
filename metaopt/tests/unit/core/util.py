5146a8c095463864749f18ada5cb62e5132f9373
Bengt Lüers <bengt.lueers@gmail.com>
2014-03-17 01:31:16 +0100

    port dictionary comprehension to Python <= 2.6

--- metaopt/tests/unit/core/util.py
+++ metaopt/tests/unit/core/util.py
@@ -6,6 +6,7 @@ from __future__ import division, print_function, with_statement
 
 def get_intervals_from_function(function):
     """Returns a dictionary of parameter titles and ranges."""
-    # TODO port the following dictionary comprehension to Python 2.6 and 2.5
-    return {key: function.param_spec.params[key].interval
-            for key in function.param_spec.params.keys()}
+    intervals = dict()
+    for key in function.param_spec.params.keys():
+        intervals[key] = function.param_spec.params[key].interval
+    return intervals
