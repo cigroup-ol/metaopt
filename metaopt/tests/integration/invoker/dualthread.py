fd1b0e2de72476d92992c513531b1404d0c115ee
Bengt Lüers <bengt.lueers@gmail.com>
2014-05-19 11:37:21 +0200

    isolate dualthread tests

--- metaopt/tests/integration/invoker/dualthread.py
+++ metaopt/tests/integration/invoker/dualthread.py
@@ -23,104 +23,110 @@ f = f  # helps static code checkers
 failing_f = failing_f  # helps static code checkers
 
 
-def test_invoke_calls_on_result():
-    caller = Mock()
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+class TestDualThreadInvoker(object):
+    def __init__(self):
+        pass
 
-    invoker = DualThreadInvoker()
-    invoker.f = f
+    def setup(self):
+        pass
 
-    invoker.param_spec = f.param_spec
-    invoker.return_spec = None
-    # invoker.return_spec = ReturnSpec(f)  # TODO: Fix problems with equality
+    def teardown(self):
+        pass
 
-    args = ArgsCreator(f.param_spec).args()
+    def test_invoke_calls_on_result(self):
+        caller = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
 
-    invoker.invoke(caller=caller, fargs=args)
-    invoker.wait()
+        invoker = DualThreadInvoker()
+        invoker.f = f
 
-    caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
-                                        fargs=args)
+        invoker.param_spec = f.param_spec
+        invoker.return_spec = None
+        # invoker.return_spec = ReturnSpec(f)  # TODO: Fix problems with equality
 
+        args = ArgsCreator(f.param_spec).args()
 
-def test_invoke_multiple_times_calls_on_result():
-    caller = Mock()
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+        invoker.invoke(caller=caller, fargs=args)
+        invoker.wait()
 
-    invoker = DualThreadInvoker()
-    invoker.f = f
+        caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
+                                            fargs=args)
 
-    invoker.param_spec = f.param_spec
+    def test_invoke_multiple_times_calls_on_result(self):
+        caller = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
 
-    # invoker.return_spec = ReturnSpec(f)  # TODO: Fix problems with equality
-    invoker.return_spec = None
+        invoker = DualThreadInvoker()
+        invoker.f = f
 
-    args = ArgsCreator(f.param_spec).args()
+        invoker.param_spec = f.param_spec
 
-    invoker.invoke(caller=caller, fargs=args)
-    invoker.wait()
+        # invoker.return_spec = ReturnSpec(f)  # TODO: Fix problems with equality
+        invoker.return_spec = None
 
-    caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
-                                        fargs=args)
+        args = ArgsCreator(f.param_spec).args()
 
-    invoker.invoke(caller=caller, fargs=args)
-    invoker.wait()
+        invoker.invoke(caller=caller, fargs=args)
+        invoker.wait()
 
-    caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
-                                        fargs=args)
+        caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
+                                            fargs=args)
 
+        invoker.invoke(caller=caller, fargs=args)
+        invoker.wait()
 
-def test_invoke_different_invokers_calls_on_result():
-    test_invoke_calls_on_result()
-    test_invoke_calls_on_result()
+        caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
+                                            fargs=args)
 
+    def test_invoke_different_invokers_calls_on_result(self):
+        self.test_invoke_calls_on_result()
+        self.test_invoke_calls_on_result()
 
-def test_invoke_given_extra_args_calls_on_result_with_them():
-    invoker = DualThreadInvoker()
-    invoker.f = f
+    def test_invoke_given_extra_args_calls_on_result_with_them(self):
+        invoker = DualThreadInvoker()
+        invoker.f = f
 
-    invoker.param_spec = f.param_spec
+        invoker.param_spec = f.param_spec
 
-    # invoker.return_spec = ReturnSpec(f)  # TODO: Fix problems with equality
-    invoker.return_spec = None
+        # invoker.return_spec = ReturnSpec(f)  # TODO: Fix problems with equality
+        invoker.return_spec = None
 
-    caller = Mock()
+        caller = Mock()
 
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
 
-    data = object()
+        data = object()
 
-    args = ArgsCreator(f.param_spec).args()
+        args = ArgsCreator(f.param_spec).args()
 
-    invoker.invoke(caller, args, data=data)
-    invoker.wait()
+        invoker.invoke(caller, args, data=data)
+        invoker.wait()
 
-    caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
-                                        fargs=args, data=data)
+        caller.on_result.assert_called_with(value=ReturnValuesWrapper(None, 0),
+                                            fargs=args, data=data)
 
+    def test_invoke_calls_on_error(self):
+        invoker = DualThreadInvoker()
+        invoker.f = failing_f
 
-def test_invoke_calls_on_error():
-    invoker = DualThreadInvoker()
-    invoker.f = failing_f
+        invoker.param_spec = failing_f.param_spec
+        invoker.return_spec = None
 
-    invoker.param_spec = failing_f.param_spec
-    invoker.return_spec = None
+        caller = Mock()
 
-    caller = Mock()
+        caller.on_result = Mock()
+        caller.on_error = Mock()
 
-    caller.on_result = Mock()
-    caller.on_error = Mock()
+        args = ArgsCreator(failing_f.param_spec).args()
 
-    args = ArgsCreator(failing_f.param_spec).args()
+        invoker.invoke(caller=caller, fargs=args)
+        invoker.wait()
 
-    invoker.invoke(caller=caller, fargs=args)
-    invoker.wait()
-
-    assert not caller.on_result.called
-    assert caller.on_error.called  # TODO: Also test arguments
+        assert not caller.on_result.called
+        assert caller.on_error.called  # TODO: Also test arguments
 
 if __name__ == '__main__':
     nose.runmodule()
