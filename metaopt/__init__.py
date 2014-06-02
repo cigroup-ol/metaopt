78f8c4386c7e3ec2a8ee007c85361aea65fb74b5
Bengt Lüers <Bengt.Lueers@gmail.com>
2014-06-02 15:12:41 +0200

    Update __init__.py

--- metaopt/__init__.py
+++ metaopt/__init__.py
@@ -2,10 +2,10 @@
 """
 Root package of MetaOpt.
 """
-__author__ = 'Renke Grunwald, Bengt Lüers, Jendrik Poloczek, Justin Heinermann'
-__author_email__ = 'info@metaopt.org'
+__author__ = 'Renke Grunwald, Bengt Lüers, Jendrik Poloczek, Justin Heinermann, Oliver Kramer'
+__author_email__ = 'justin.heinermann@informatik.uni-oldenburg.de'
 __license__ = 'BSD'
 # __maintainer__ = "first last"
 # __maintainer_email__ = "first.last@example.com"
-__url__ = 'http://organic-es.tumblr.com/'
-__version__ = '0.1.0.0'
+__url__ = 'https://pypi.python.org/pypi/metaopt/'
+__version__ = '0.1.0'
