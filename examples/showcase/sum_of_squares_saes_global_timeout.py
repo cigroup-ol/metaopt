f8c478ffd0105f7e6eabf827fb053d9549827a66
Justin P. Heinermann <justin.heinermann@informatik.uni-oldenburg.de>
2014-06-03 09:13:50 +0200

    Removed unneeded imports in sum of squares example

--- examples/showcase/sum_of_squares_saes_global_timeout.py
+++ examples/showcase/sum_of_squares_saes_global_timeout.py
@@ -14,15 +14,6 @@ from metaopt.core.returnspec.util.decorator import minimize
 from metaopt.core.paramspec.util import param
 from metaopt.core.optimize.optimize import optimize
 
-# Third Party
-from sklearn.datasets import make_friedman1
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.base import BaseEstimator 
-from sklearn.base import RegressorMixin
-from sklearn.metrics import mean_squared_error as mse
-import numpy as np
-import math
-
 n_params=15
 
 """
