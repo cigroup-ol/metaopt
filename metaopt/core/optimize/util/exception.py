6a4cf73630cf6c42ca8b8142801ead05a1490f92
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2014-06-16 13:27:32 +0200

    added WrongArgsException and OptimizerException

--- metaopt/core/optimize/util/exception.py
+++ metaopt/core/optimize/util/exception.py
@@ -2,6 +2,15 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals, with_statement
 
+class OptimizerException(Exception):
+    """Indicates general error in optimizer."""
+    def __init__(self, message=None):
+        super(OptimizerException, self).__init__(message)
+
+class WrongArgumentTypeException(Exception):
+    """Indicates wrong type of parameters."""
+    def __init__(self, message=None):
+        super(WrongArgumentTypeException, self).__init__(message)
 
 class GlobalTimeoutError(Exception):
     """Indicates that the the global timeout has occurred."""
